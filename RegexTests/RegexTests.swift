d84bd489f5b7b610abbfe406d2ed4b18b091cbf1
bryn austin bellomy <bryn.bellomy@gmail.com>
2015-06-03 16:04:59 -0500

    adding a few simple tests

--- RegexTests/RegexTests.swift
+++ RegexTests/RegexTests.swift
@@ -19,4 +19,26 @@ class RegexTests: XCTestCase
     override func tearDown() {
         super.tearDown()
     }
+    
+    func testStringReplaceRegex() {
+        let name = "Winnie the Pooh"
+        let darkName = name.replaceRegex("Winnie the ([a-zA-Z]+)", with: "Darth $1")
+        XCTAssertEqual(darkName, "Darth Pooh")
+    }
+    
+    func testStringGrep() {
+        let name = "Winnie the Pooh"
+        let matchResult = name.grep("\\s+([a-z]+)\\s+")
+        XCTAssertTrue(matchResult.boolValue)
+        XCTAssertEqual(matchResult.searchString, "Winnie the Pooh")
+        
+        XCTAssertEqual(matchResult.captures.count, 2)
+        XCTAssertEqual(matchResult.captures[0], " the ")
+        XCTAssertEqual(matchResult.captures[1], "the")
+    }
+    
+    func testMapFunction() {
+        let replaced = map("Winnie the Pooh" =~ Regex("([a-zA-Z]+)\\s+(the)(.*)"), "$2 $1")
+        XCTAssertEqual(replaced, "the Winnie")
+    }
 }
